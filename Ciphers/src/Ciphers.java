fe097224548054d85e830ae879f56a6a7d204d9f
Marietta Asemwota <marietta.asemwota@gmail.com>
2018-09-16 21:09:20 -0700

    added frequency count

--- Ciphers/src/Ciphers.java
+++ Ciphers/src/Ciphers.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 public class Ciphers {
 	
 	public static void main(String [] args)
@@ -10,14 +12,60 @@ public class Ciphers {
 		
 		sample = "Marietta"; 
 		caeser(sample);
+		
+		//countCharacters("Marietta");
+		countCharacters("FCGUNKBHEJGCLK");
+		
+		//simpleKeySearch("PDULHWWD");
+		//simpleKeySearch("FCGUNKBHEJGCLK");
+		//simpleKeySearch("IESRNATOLUSECA");
+	}
+	
+	public static void countCharacters(String input)
+	{
+//		int[] count = new int [26];
+//		
+//		for(int i = 0; i < input.length(); i++)
+//        {
+//            int index = getNum(input.charAt(i));
+//            System.out.println(index);
+//            //count[index]++;
+//        }
+//		
+//		 for (int i = 0; i < 26; i++) {
+//	            System.out.println(getChar(i) + " " + count[i]);
+//	        }
+		
+		Map<Character,Integer> frequencies = new HashMap<>();
+		for (char ch : input.toCharArray()) 
+		   frequencies.put(ch, frequencies.getOrDefault(ch, 0) + 1);
+		
+		for (Character ch : frequencies.keySet())
+		{
+			System.out.println(ch + " " + frequencies.get(ch));
+		}
+        
+        
+	}
+	
+	/**
+	 * A method that tries all possible keys for simple substitution 
+	 * @param input
+	 */
+	public static void simpleKeySearch(String input)
+	{
+		for(int i = 0; i < 26; i++)
+		{
+			simple(input, i);
+		}
 	}
 	
 	/**
 	 * A simple substitution cipher
 	 * @param input - the string to encrypt
-	 * @param n - the number to translate by 
+	 * @param n - the number to shift by 
 	 */
-	public static void simple(String input, int n)
+	public static String simple(String input, int n)
 	{		
 		System.out.println(input + "; n = " + n);
 		input = input.toUpperCase();
@@ -30,6 +78,18 @@ public class Ciphers {
 		}
 		
 		System.out.println(output + "\n");
+		return output;
+	}
+	
+	/**
+	 * Given a ciphertext and an n, get back the plaintext that would have been the input 
+	 * to a simple substitution cipher with the provided n
+	 * @param input - the ciphertext
+	 * @param n - the key
+	 */
+	public static void decryptSimple(String input, int n)
+	{
+		simple(input, 0 - n); 
 	}
 	
 	/**
@@ -39,19 +99,7 @@ public class Ciphers {
 	 */
 	public static void caeser(String input)
 	{
-//		System.out.println(input);
-//		input = input.toUpperCase();
-//		
-//		String output = "";
-//		for(int i = 0; i < input.length(); i++) 
-//		{
-//			char c = input.charAt(i);
-//			int num = getNum(c);
-//			output += getChar(num + 3);
-//		}
-//		
-//		System.out.println(output + "\n");
-		simple(input, 3);
+		String ct = simple(input, 3);
 	}
 	
 	public static int getNum(char c) {
@@ -59,7 +107,7 @@ public class Ciphers {
     }
 	
 	public static char getChar(int num) {
-		return (char) (num + 'A'); 
+		return (char) ((num % 26) + 'A'); 
 	}
 
 }
